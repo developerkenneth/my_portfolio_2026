603c655d08c8a00e1b07f705a54c41b4ceed2de7
developerkenneth <107970456+developerkenneth@users.noreply.github.com>
2026-03-25 12:07:45 +0100

    added contact page functionality to send contact information to my database

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,39 +1,43 @@
 const projects = [
-    {
-        name: "Iride Car Booking Company",
-        description: "A comprehensive car rental and booking platform featuring a streamlined reservation system and real-time fleet management.",
-        url: "https://iride.ng",
-        picture: "assets/img/iride.png",
-        stacks: ["Laravel", "Javascript", "PHP", "Tailwind"]
-    },
-    {
-        name: "Good Luck Goods E-commerce",
-        description: "A dynamic online marketplace built for speed, featuring a high-performance React frontend and a robust Laravel backend for secure transactions.",
-        url: "https://shop.goodlucks.co",
-        picture: "https://shop.goodlucks.co/assets/hero-img-Ci7wdoPu.jpg",
-        stacks: ["Laravel", "React", "PHP"]
-    },
-    {
-        name: "Diamond Shipping Landing Page",
-        description: "A modern, high-conversion landing page for a logistics firm, optimized for mobile responsiveness and fast load times.",
-        url: "https://www.diamondshipping.org.ng/",
-        picture: "https://www.diamondshipping.org.ng/assets/img/hero.jpeg",
-        stacks: ["HTML", "Tailwind CSS"]
-    },
-    {
-        name: "Moving Forward Oil Company",
-        description: "A sleek, professional corporate landing page for an energy enterprise, focusing on clean layout and brand identity.",
-        url: "", // Note: Added a placeholder for the URL if available
-        picture: "assets/img/movingforward.png",
-        stacks: ["HTML", "React", "Tailwind"]
-    }
+  {
+    name: "Iride Car Booking Company",
+    description: "A comprehensive car rental and booking platform featuring a streamlined reservation system and real-time fleet management.",
+    url: "https://iride.ng",
+    picture: "assets/img/iride.png",
+    stacks: ["laravel", "javascript", "php", "tailwind"]
+  },
+  {
+    name: "Good Luck Goods E-commerce",
+    description: "A dynamic online marketplace built for speed, featuring a high-performance React frontend and a robust Laravel backend for secure transactions.",
+    url: "https://shop.goodlucks.co",
+    picture: "https://shop.goodlucks.co/assets/hero-img-Ci7wdoPu.jpg",
+    stacks: ["laravel", "react", "php"]
+  },
+  {
+    name: "Diamond Shipping Landing Page",
+    description: "A modern, high-conversion landing page for a logistics firm, optimized for mobile responsiveness and fast load times.",
+    url: "https://www.diamondshipping.org.ng/",
+    picture: "https://www.diamondshipping.org.ng/assets/img/hero.jpeg",
+    stacks: ["html", "tailwind css"]
+  },
+  {
+    name: "Moving Forward Oil Company",
+    description: "A sleek, professional corporate landing page for an energy enterprise, focusing on clean layout and brand identity.",
+    url: "", // Note: Added a placeholder for the URL if available
+    picture: "assets/img/movingforward.png",
+    stacks: ["html", "react", "tailwind"]
+  }
 ]
 
 
 const projectGrid = document.querySelector("#project-grid");
+const filterBtns = document.querySelectorAll(".filter-btn");
 
-let projectContent = "";
-projects.forEach((project) => {
+
+const displayProjects = (projects) => {
+
+  let projectContent = "";
+  projects.forEach((project) => {
 
     const allStacks = project.stacks.map(stack => `
     <span class="px-2 py-1 text-[10px] font-bold tracking-wider uppercase bg-blue-800 text-white rounded">
@@ -66,7 +70,7 @@ projects.forEach((project) => {
             </p>
             <div class="flex items-center gap-4 mt-4">
               <a href="${project.url}"
-                class="flex-1 bg-primary text-white text-xs font-bold py-2.5 rounded-lg hover:bg-primary/90 transition-colors"
+                class="flex-1 px-2 inline-block bg-primary text-white text-xs font-bold py-2.5 rounded-lg hover:bg-primary/90 transition-colors"
               >
                 View Case Study
               </a>
@@ -79,6 +83,39 @@ projects.forEach((project) => {
           </div>
         </div>
         `;
-});
+  });
 
-projectGrid.innerHTML = projectContent;
+  projectGrid.innerHTML = projectContent;
+}
+
+displayProjects(projects);
+
+// filter projects based on tags
+
+filterBtns.forEach(btn => {
+  btn.addEventListener("click", e => {
+    const clickedBtn = e.currentTarget;
+    const tag = clickedBtn.dataset.tag;
+
+    // 1. Reset all buttons to the "inactive" gray state
+    filterBtns.forEach(b => {
+      b.classList.remove("bg-primary", "text-white", "font-semibold");
+      b.classList.add("bg-slate-200", "dark:bg-slate-800", "font-medium");
+    });
+
+    // 2. Set the clicked button to the "active" blue state
+    clickedBtn.classList.add("bg-primary", "text-white", "font-semibold");
+    clickedBtn.classList.remove("bg-slate-200", "dark:bg-slate-800", "font-medium");
+
+    // 3. Filter Logic
+    if (tag === "allProjects") {
+      displayProjects(projects);
+    } else {
+      const filtered = projects.filter(project => {
+        // Use optional chaining (?.) and lowerCase to prevent errors
+        return project.stacks?.some(stack => stack.toLowerCase() === tag.toLowerCase());
+      });
+      displayProjects(filtered);
+    }
+  });
+});
