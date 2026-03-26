c3b2432021e6c75c15e79c08742c4e2b445d5da8
developerkenneth <107970456+developerkenneth@users.noreply.github.com>
2026-03-26 14:07:59 +0100

    added db configurations to help me connect to the database

---
+++ admin/api/index.php
@@ -0,0 +1,5 @@
+<?php
+
+// API endpoint for handling AJAX requests from the admin inquiries page
+require_once '../init/core.php';
+require_once ROOT_PATH . '/init/Db.php';
---
+++ admin/init/core.php
@@ -0,0 +1,4 @@
+<?php
+
+define('CORE_PATH', __DIR__);
+define('ROOT_PATH', dirname(CORE_PATH));
